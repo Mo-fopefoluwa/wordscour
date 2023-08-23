eb95d62a220e70990aa6f9bf9abb305f4da7b1e8
Mo-fopefoluwa <fopefaokunla@gmail.com>
2023-08-23 14:32:40 +0100

    fops

--- src/component/Content/Content.jsx
+++ src/component/Content/Content.jsx
@@ -9,7 +9,7 @@ const Content = ({ partOfSpeech, definitions, synonyms }) => {
           <hr className="w-full ml-10" />
         </div>
         <div className="text-left ">
-          <p className="text-gray-800 dark:text-gray-500 mt-3 font-semibold">
+          <p className="text-gray-800 dark:text-gray-400 mt-3 font-semibold">
             Meaning
           </p>
           <ul className="list-disc px-10 text-gray-800 dark:text-gray-500 text-sm space-y-2">
@@ -17,7 +17,7 @@ const Content = ({ partOfSpeech, definitions, synonyms }) => {
               <li key={index}>{def.definition}</li>
             ))}
           </ul>
-          <p className="text-gray-800 dark:text-gray-500 mt-6 font-semibold">
+          <p className="text-gray-800 dark:text-gray-400 mt-6 font-semibold">
             Example
           </p>
           <ul className="list-disc px-10 mt-2 text-gray-800 dark:text-gray-500 text-sm space-y-2">
@@ -25,7 +25,7 @@ const Content = ({ partOfSpeech, definitions, synonyms }) => {
               <li key={index}>{defs.example}</li>
             ))}
           </ul>
-          <p className="mt-4 text-gray-800 text-sm font-semibold">
+          <p className="mt-4 text-gray-800 dark:text-gray-400 text-sm font-semibold">
             Synonyms:{" "}
             <span className="text-indigo-500 flex flex-row gap-4 mt-2">
               {synonyms.map((item, index) => (
